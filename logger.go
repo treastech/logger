f9a4f85389e213305dff0031be61bcbc071b9f8a
Deepak Sharma <sadysnaat@gmail.com>
2018-07-06 04:48:01 +0530

    Comments for exported function Logger

--- logger.go
+++ logger.go
@@ -7,6 +7,9 @@ import (
 	"time"
 )
 
+// Logger is a middleware that logs the start and end of each request, along
+// with some useful data about what was requested, what the response status was,
+// and how long it took to return.
 func Logger(l *zap.Logger) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
